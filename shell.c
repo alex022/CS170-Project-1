6dc59f5904472abf107d60380f4bd343f647bcb6
leinad0213 <danielkwak@umail.ucsb.edu>
2013-04-20 00:26:00 -0700

    kinda...8===D

--- shell.c
+++ shell.c
@@ -7,19 +7,26 @@
 int main(void)
 {
 	char input[CLENGTH];
+	char command[5] = "";
+	char* split;
 
-  	while(1)
-       {
+	while(1)
+	{
       	printf("sish:> ");
-      	fgets(input, CLENGTH, stdin);
-	input[strlen(input)-1] = '\0';
+		fgets(input, CLENGTH, stdin);
+		input[strlen(input)-1] = '\0';
 
-      
-      	if(strcmp(input, "exit") == 0)
-	  break;
+		//split = strtok(input, " ");
+		//if(split != NULL)
+			//strcpy(command, split);
 
+    	if(strcmp(input, "exit") == 0)
+	  			break;
 
-       
+		if(strcmp(input, "lm") == 0)
+			printf("ERROR: exec failed\n");
+
+		system(input); //Works for the most part and is extremely convenient, but we need 					   //to deal with spaces and output adjustments. Hope the prof lets 						   //us use this function.
     }
 
   return 0;
