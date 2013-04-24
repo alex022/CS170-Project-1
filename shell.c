35e9bc400a454ba67660da99ba1fd6303273e1c9
alex022 <alex.ch776@gmail.com>
2013-04-23 20:25:09 -0700

    mmmm

--- shell.c
+++ shell.c
@@ -2,15 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 #define CLENGTH 1024
+#define NONE 0
+#define PIPE 1
+#define BACKGROUND 2
+#define OUT 3
+#define IN 4
 
 int argCount(char*);
-void parse(char*, char**);
+void parse(char*);
+void op(char **, int, char**);
 
 int main(void)
-{
-  char input[CLENGTH];
+{  
+  int delim = NONE;
+  int command;
+  char *input = (char*)malloc(sizeof(char)*CLENGTH);
+  char *commands[CLENGTH];
+  char *strptr = NULL;
+  char cwd[CLENGTH];
+
+  while(1)
+    {
+      printf("sish:> ");
+      delim = NONE;
+      getcwd(cwd, CLENGTH);
+      fgets(input, CLENGTH, stdin);
+      if(strcmp(input, "exit\n")==0)
+	exit(0);
+       
+      command = split(input, commands, &delim, &strptr);
+      if(strcmp(*commands, "cd") == 0)
+	{
+	  chdir(commands[1]);
+	}
+      else
+	op(commands, delim, &strptr);
+    }
+
+
+
+
+    
+  /* char input[CLENGTH];
   char* path = "/bin/";
   char pipe[100][100]; 
   char* split;
@@ -74,11 +110,192 @@ int main(void)
 	wait(NULL); //Parent process
 	  
       }
-	
+  */
   return 0;
 }
 
-int argCount(char* buffer)
+int split(char* input, char* commands[], int *delim, char **strptr)
+{
+  int command = 0;
+  int end = 0;
+  char *sptr = input;
+
+  while((*sptr != '\0') && (!end))
+    {
+      *commands = sptr;
+      command++;
+      while((*sptr != ' ') && (*sptr != '\0') && (*sptr != '\n') && (!end))
+	{
+	  if(*sptr == '>')
+	    {
+	      *delim = OUT;
+	      *commands = '\0';
+	      sptr++;
+	      while(*sptr == ' ')
+		sptr++;
+
+	      *strptr = sptr;
+	      parse(*strptr);
+	      end = 1;	      
+	    }
+
+	  if(*sptr == '<')
+	    {
+	      *delim = IN;
+	      *commands = '\0';
+	      sptr++;
+	      while(*sptr == ' ')
+		sptr++;
+
+	      *strptr = sptr;
+	      parse(*strptr);
+	      end = 1;	      
+	    }
+	    
+	  if(*sptr == '|')
+	    {
+	      *delim = PIPE;
+	      *commands = '\0';
+	      sptr++;
+	      while(*sptr == ' ')
+		sptr++;
+
+	      *strptr = sptr;
+	      end = 1;	      
+	    }	    
+	  
+	  if(*sptr == '&')
+	    *delim = BACKGROUND;
+	
+      sptr++;
+    }
+  while(((*sptr == ' ') || (*sptr == '\n')) && (!end))
+    {
+      *sptr = '\0';
+      sptr++;
+    }
+    commands++;
+  }
+
+  *commands = '\0';
+  return command;
+}
+
+
+void parse(char *strptr)
+{
+  while((*strptr != ' ') && (*strptr != '\n'))
+	strptr++;
+
+  *strptr = '\0';
+}
+
+void op(char** commands, int delim, char** strptr)
+{
+
+  pid_t pid, pidx;
+  FILE *fp;
+  int m = NONE;
+  int command;
+  int status, statusx;
+  char *commandsx[CLENGTH];
+  char* strptrx = NULL;
+  int Pipe[2];
+  
+  if(m==PIPE)
+    {
+      if(pipe(Pipe))
+	{
+	  printf("ERROR: Exec failed.\n");
+	  exit(-1);
+	}
+      split(*strptr, commandsx, &m, &strptrx);
+    }
+
+  pid = fork();
+  if(pid<0)
+    {
+       printf("ERROR: Exec failed.\n");
+       exit(-1);
+    }
+  else if(pid==0)
+    {
+      if(delim==OUT)
+	{
+	  fp = fopen(*strptr, "w+");
+	  dup2(fileno(fp), 1);	
+	}
+      if(delim==IN)
+	{
+	  fp = fopen(*strptr, "r");
+	  dup2(fileno(fp), 0);
+	}
+      if(m==PIPE)
+	{
+	  close(Pipe[0]);
+	  dup2(Pipe[1], fileno(stdout));
+	  close(Pipe[1]);
+	}
+
+      int rv = execvp(*commands, commands);
+      if(rv==-1)
+	printf("ERROR: Execution failed.\n");
+    }
+  else
+    {
+      if(m==BACKGROUND);
+      else if(m==PIPE)
+	{
+	  waitpid(pid, &status, 0);
+	  pidx = fork();
+	  if(pidx < 0)
+	    {
+	      printf("ERROR: Fork failed.\n");
+	      exit(-1);
+	    }
+	  else if(pidx==0)
+	    {
+	      close(Pipe[1]);
+	      dup2(Pipe[0], fileno(stdin));
+	      close(Pipe[0]);
+	      int rv = execvp(*commandsx, commandsx);
+	      if(rv==-1)
+		printf("ERROR: Execution failed.\n");
+	    }
+	  else
+	    {
+	       wait(NULL);
+	       waitpid(pid, &status, 0);
+	       waitpid(pidx, &statusx, 0);
+	      close(Pipe[0]);
+	      close(Pipe[1]);
+	    }
+
+	}
+      else
+	waitpid(pid, &status, 0);
+        wait(NULL);
+    }
+}
+
+
+/*void parse(char* in, char** arguments)
+{
+  char* token;
+  int i = 0;
+
+  token = strtok(in, " ");
+  while(token != NULL)
+    {
+      arguments[i] = (char*)malloc((strlen(token)*sizeof(char*)));
+      strcpy(arguments[i], token);
+      token = strtok(NULL, " ");
+      i++;
+    }
+}
+*/
+
+/*int argCount(char* buffer)
 {
   int args = 0;
   char* token;
@@ -98,21 +315,4 @@ int argCount(char* buffer)
     }
   return args;
 }
-
-void parse(char* in, char** arguments)
-{
-  char* token;
-  int i = 0;
-
-  token = strtok(in, " ");
-  while(token != NULL)
-    {
-      arguments[i] = (char*)malloc((strlen(token)*sizeof(char*)));
-      strcpy(arguments[i], token);
-      token = strtok(NULL, " ");
-      i++;
-    }
-}
-
-
-
+*/
